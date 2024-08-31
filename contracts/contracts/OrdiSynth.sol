b96137a0c19ed2ccebc9db26144365d2448e564a
wr1159 <chuweiron18@gmail.com>
2024-08-31 15:52:03 +0800

    Draft RuneToken integration

--- contracts/contracts/OrdiSynth.sol
+++ contracts/contracts/OrdiSynth.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
-import "./ERC1155.sol";
+import "./RuneToken.sol";
 import "./SynthToken.sol";
 
 contract OrdiSynth is ERC1155Holder {
@@ -14,78 +14,68 @@ contract OrdiSynth is ERC1155Holder {
       router = IUniswapV2Router02(_router);
   }
   mapping (address => address) public synthAddressByContractAddress;
-  mapping (address => uint256) public ownedErc1155ByContractAddress;
+  mapping (address => uint256) public ownedRuneByContractAddress;
   // used to keep track of what is available to be redeemed
-  mapping (address => mapping(uint => uint256)) public availableErc1155ByContractAddressAndTokenId;
+  mapping (address => mapping(uint => uint256)) public availableRuneByContractAddressAndTokenId;
 
-  // depositForSynth is a function that allows users to deposit erc1155 tokens to mint a synth
+  // depositForSynth is a function that allows users to deposit rune tokens to mint a synth
   // Ensure that the user has approved transfer
   function depositForSynth(address contractAddress, uint tokenId, uint256 erc1155Amount) public {
     SynthToken synthToken;
-    IERC1155 erc1155 = IERC1155(contractAddress);
-    // 1. transfer erc1155 tokens to this contract
-    erc1155.safeTransferFrom(msg.sender, address(this), tokenId, erc1155Amount, "");
+    RuneToken rune = RuneToken(contractAddress);
+    // 1. transfer rune tokens to this contract
+    rune.safeTransferFrom(msg.sender, address(this), tokenId, erc1155Amount, "");
 
     // 2. set the token address to the synthByContractAddress mapping
     if (synthAddressByContractAddress[contractAddress] != address(0)) {
       synthToken = SynthToken(synthAddressByContractAddress[contractAddress]);
     } else {
-      MockERC1155 mockErc1155 = MockERC1155(contractAddress);
-      if (address(mockErc1155) == address(0)) {
-        synthToken = new SynthToken("syntheticERC", "sERC", address(this));
-      } else {
-        synthToken = new SynthToken(string.concat("synthetic", mockErc1155.name()), string.concat("s", mockErc1155.symbol()), address(this));
-      }
+      synthToken = new SynthToken(string.concat("synthetic", rune.getTokenInfo(tokenId, msg.sender).name), string.concat("s", rune.getTokenInfo(tokenId, msg.sender).symbol), address(this));
       synthAddressByContractAddress[contractAddress] = address(synthToken);
     }
     // 3. mint synth tokens to the user
-    ownedErc1155ByContractAddress[contractAddress] += erc1155Amount;
-    availableErc1155ByContractAddressAndTokenId[contractAddress][tokenId] += erc1155Amount;
+    ownedRuneByContractAddress[contractAddress] += erc1155Amount;
+    availableRuneByContractAddressAndTokenId[contractAddress][tokenId] += erc1155Amount;
 
     uint256 mintAmount = erc1155Amount * (10**synthToken.decimals());
     synthToken.mint(msg.sender, mintAmount);
     return;
   }
 
-  // redeemSynth is a function that allows users to redeem synth tokens for their erc1155 tokens
+  // redeemSynth is a function that allows users to redeem synth tokens for their rune tokens
   function redeemSynth(address contractAddress, uint tokenId, uint256 erc1155Amount) public {
     SynthToken synthToken = SynthToken(synthAddressByContractAddress[contractAddress]);
-    IERC1155 erc1155 = IERC1155(contractAddress);
+    RuneToken rune = RuneToken(contractAddress);
     // 1. burn synth tokens from the user
     uint256 synthAmount = erc1155Amount * (10**synthToken.decimals());
     synthToken.transferFrom(msg.sender, address(this), synthAmount);
     synthToken.burn(synthAmount);
 
-    ownedErc1155ByContractAddress[contractAddress] -= erc1155Amount;
-    availableErc1155ByContractAddressAndTokenId[contractAddress][tokenId] -= erc1155Amount;
-    // 2. transfer erc1155 tokens to the user
-    erc1155.setApprovalForAll(msg.sender, true);
-    erc1155.safeTransferFrom(address(this), msg.sender, tokenId, erc1155Amount, "");
+    ownedRuneByContractAddress[contractAddress] -= erc1155Amount;
+    availableRuneByContractAddressAndTokenId[contractAddress][tokenId] -= erc1155Amount;
+    // 2. transfer rune to the user
+    rune.setApprovalForAll(msg.sender, true);
+    rune.safeTransferFrom(address(this), msg.sender, tokenId, erc1155Amount, "");
     return;
   }
 
   // addLiquidityToRouter is a function that allows users to add liquidity to the uniswap pool
   function addLiquidityToRouter(address contractAddress, uint tokenId, uint256 erc1155Amount) public payable {
     SynthToken synthToken;
-    IERC1155 erc1155 = IERC1155(contractAddress);
+    RuneToken rune = RuneToken(contractAddress);
     // 1. transfer erc1155 tokens to this contract
-    erc1155.safeTransferFrom(msg.sender, address(this), tokenId, erc1155Amount, "");
+    rune.safeTransferFrom(msg.sender, address(this), tokenId, erc1155Amount, "");
 
     // 2. set the token address to the synthByContractAddress mapping
     if (synthAddressByContractAddress[contractAddress] != address(0)) {
       synthToken = SynthToken(synthAddressByContractAddress[contractAddress]);
       
     } else {
-      MockERC1155 mockErc1155 = MockERC1155(contractAddress);
-      if (address(mockErc1155) == address(0)) {
-        synthToken = new SynthToken("syntheticERC", "sERC", address(this));
-      } else {
-        synthToken = new SynthToken(string.concat("synthetic", mockErc1155.name()), string.concat("s", mockErc1155.symbol()), address(this));
-      }
+      synthToken = new SynthToken(string.concat("synthetic", rune.getTokenInfo(tokenId, msg.sender).name), string.concat("s", rune.getTokenInfo(tokenId, msg.sender).symbol), address(this));
       synthAddressByContractAddress[contractAddress] = address(synthToken);
     }
-    ownedErc1155ByContractAddress[contractAddress] += erc1155Amount;
-    availableErc1155ByContractAddressAndTokenId[contractAddress][tokenId] += erc1155Amount;
+    ownedRuneByContractAddress[contractAddress] += erc1155Amount;
+    availableRuneByContractAddressAndTokenId[contractAddress][tokenId] += erc1155Amount;
 
     uint256 mintAmount = erc1155Amount * (10**synthToken.decimals());
     synthToken.mint(address(this), mintAmount);
@@ -103,11 +93,11 @@ contract OrdiSynth is ERC1155Holder {
     return;
   }
 
-  // swapTokenFor1155 is a function that allows users to swap any ERC20 tokens on the uniswap pool for synth tokens
-  // which will be redeemed for erc1155 tokens
+  // swapTokenFor1155 is a function that allows users to swap any rBTC on the uniswap pool for synth tokens
+  // which will be redeemed for rune tokens
   function swapTokenFor1155(address contractAddress, uint tokenId, uint256 erc1155Amount) public payable {
     SynthToken synthToken = SynthToken(synthAddressByContractAddress[contractAddress]);
-    IERC1155 erc1155 = IERC1155(contractAddress);
+    RuneToken rune = RuneToken(contractAddress);
     // 1. swap tokens for synth tokens
     uint256 synthAmount = erc1155Amount * (10**synthToken.decimals());
     address[] memory path = new address[](2);
@@ -123,11 +113,11 @@ contract OrdiSynth is ERC1155Holder {
     // 2. burn synth tokens 
     synthToken.burn(synthAmount);
 
-    ownedErc1155ByContractAddress[contractAddress] -= erc1155Amount;
-    availableErc1155ByContractAddressAndTokenId[contractAddress][tokenId] -= erc1155Amount;
+    ownedRuneByContractAddress[contractAddress] -= erc1155Amount;
+    availableRuneByContractAddressAndTokenId[contractAddress][tokenId] -= erc1155Amount;
     // 3. transfer erc1155 tokens to the user
-    erc1155.setApprovalForAll(msg.sender, true);
-    erc1155.safeTransferFrom(address(this), msg.sender, tokenId, erc1155Amount, "");
+    rune.setApprovalForAll(msg.sender, true);
+    rune.safeTransferFrom(address(this), msg.sender, tokenId, erc1155Amount, "");
     return;
   }
 
